582881223c6702daf7bd5190953c48f3e0e679b2
167716 <101186863+167716@users.noreply.github.com>
2022-03-22 12:40:23 +0000

    tragedia

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
+import java.lang.Math;
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Kalkulator");
+    System.out.println("Wybierz funkcje:");
+    System.out.println("1 - potegowanie");
+    System.out.println("2 - suma pierwiastków");
+    System.out.println("3 - najwieksza liczba");
+    System.out.println("4 - ");
+
+    
+    Scanner funkcja = new Scanner(System.in); 
+    int x=funkcja.nextInt();
+  
+    switch(x){
+
+    case 1:
+      System.out.println("Liczba A do potegi B);
+        
+    break;
+        
+    }
   }
 }
